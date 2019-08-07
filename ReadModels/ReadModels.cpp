9e7de122af4242d9785c82feee130c7e841d5903
pipi953 <pipi953@qq.com>
2019-08-07 21:31:11 +0900

    基础版（个人编写）

--- ReadModels/ReadModels.cpp
+++ ReadModels/ReadModels.cpp
@@ -1,13 +1,80 @@
 ﻿// ReadModels.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include "pch.h"
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+
 #include <iostream>
+#include <vector>
+
+std::vector<float> ve;
+
+void readModels(char const* path) {
+
+	FILE* fp;
+	fopen_s(&fp, "ModelData.h", "w");
+	fprintf(fp, "float ModelVerts[] = {\n");
+
+	Assimp::Importer importer;
+	importer.SetPropertyInteger(AI_CONFIG_PP_SBP_REMOVE, aiPrimitiveType_LINE | aiPrimitiveType_POINT);
+	importer.SetPropertyInteger(AI_CONFIG_PP_PTV_NORMALIZE, true);
+
+	unsigned flags = aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals;
+
+	const aiScene* scene = importer.ReadFile(path, flags);
+
+	if (!scene) {
+		printf("model load error.\n");
+		system("pause");
+		return;
+	}
+	const aiMesh* mesh = scene->mMeshes[0];
+
+	uint32_t numFaces = 0;
+	std::unique_ptr<uint32_t[]> vertsIndex(new uint32_t[3 * mesh->mNumFaces]);
+
+	numFaces = mesh->mNumFaces;
+	for (unsigned i = 0; i < numFaces; i++) {
+		vertsIndex[3 * i] = mesh->mFaces[i].mIndices[0];
+		vertsIndex[3 * i + 1] = mesh->mFaces[i].mIndices[1];
+		vertsIndex[3 * i + 2] = mesh->mFaces[i].mIndices[2];
+	}
+
+	std::cout << "~~~~~~~~~~" << mesh->mNumFaces << " " << std::endl;
+
+	for (unsigned i = 0; i < mesh->mNumVertices; i++) {
+		aiVector3D tmp = mesh->mVertices[vertsIndex[i]];
+		ve.push_back(tmp.x);
+		ve.push_back(tmp.y);
+		ve.push_back(tmp.z);
+
+		ve.push_back(4.0f);
+		ve.push_back(0.0f);
+		ve.push_back(1.0f);
+		ve.push_back(0.0f);
+
+		fprintf(fp, "%ff, %ff, %ff, ", tmp.x, tmp.y, tmp.z);
+
+		fprintf(fp, "%.0f.f, %ff, %ff, %ff,      ", 4.0, 0.0f, 1.0f, 0.0f);
+		if ((i + 1) % 3 == 0)
+			fprintf(fp, "\n");
+	}
+
+	fprintf(fp, "};\n\n");
+
+
+}
 
 int main()
 {
+	readModels("models/Stanford_Bunny/bunny.obj");
+
     std::cout << "Hello World!\n"; 
 }
+
+
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
